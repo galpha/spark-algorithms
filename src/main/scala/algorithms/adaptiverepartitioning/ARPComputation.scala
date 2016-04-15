d823767442803a96ba4f1137f3706e3bc57ef311
Kevin Gomez <k.gomez@freenet.de>
2016-04-15 16:33:56 +0200

    init

---
+++ src/main/scala/algorithms/adaptiverepartitioning/ARPComputation.scala
@@ -0,0 +1,8 @@
+package algorithms.adaptiverepartitioning
+
+/**
+  * Created by galpha on 4/15/16.
+  */
+class ARPComputation {
+
+}
---
+++ src/main/scala/algorithms/labelpropagation/labelpropagation.scala
@@ -0,0 +1,12 @@
+package algorithms.labelpropagation
+
+import org.apache.spark.graphx.GraphLoader
+
+/**
+  * Created by galpha on 4/15/16.
+  */
+object labelpropagation {
+
+
+
+}
